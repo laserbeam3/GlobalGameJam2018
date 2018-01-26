ec8972fb63c98bd5b210ee1cd25fdc4dc35bda91
Catalin Balan <cba@trifork.com>
2018-01-26 19:38:54 +0200

    Character can walk on inclined plane

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float xSpeed = 4.0f;
+    public float ySpeed = 4.0f;
+
+    void Start ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        float delta = Time.deltaTime;
+
+        float xInput = Input.GetAxisRaw("Horizontal") * xSpeed * delta;
+        float yInput = Input.GetAxisRaw("Vertical")   * ySpeed * delta;
+
+        Transform logicalPlane = World.instance.logicalPlane.transform;
+        Vector3 forward = logicalPlane.right;
+        Vector3 sideways = logicalPlane.up;
+
+        Vector3 movement = forward * xInput + sideways * yInput;
+
+        transform.localPosition += movement;
+
+    }
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class World : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject logicalPlane;
+
+    public static World instance { get; private set; }
+
+    public void Start()
+    {
+        if (instance == null) instance = this;
+    }
+}
